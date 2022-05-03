48a03ee8bb51247561cb5bc29084dcb7bf72ccbe
gsga <ggarciaa@gmail.com>
2022-05-03 15:51:28 -0300

    A

---
+++ simple.sql
@@ -0,0 +1,35 @@
+DROP DATABASE IF EXISTS `simple`;
+
+CREATE DATABASE IF NOT EXISTS `simple` DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci;
+
+DROP USER `springuser` @`%`;
+
+CREATE USER `springuser` @`%` IDENTIFIED BY 'ThePassword';
+
+GRANT USAGE ON *.* TO `springuser` @`%`;
+
+GRANT ALL PRIVILEGES ON `simple`.* TO ' springuser ' @' % ';
+
+USE `simple`;
+
+DROP TABLE IF EXISTS `user`;
+
+CREATE TABLE IF NOT EXISTS `user` (
+    `id` int UNSIGNED NOT NULL AUTO_INCREMENT,
+    `email` varchar(255) DEFAULT NULL,
+    `name` varchar(255) DEFAULT NULL,
+    PRIMARY KEY (`id`)
+) ENGINE = InnoDB AUTO_INCREMENT = 10 DEFAULT CHARSET = utf8mb4 COLLATE = utf8mb4_0900_ai_ci;
+
+INSERT INTO
+    `user` (`id`, `email`, `name`)
+VALUES
+    (1, ' uno @gmail.com ', ' Uno '),
+    (2, ' dos @gmail.com ', ' Dos '),
+    (3, ' other @gmail.com ', ' Other '),
+    (4, ' some @gmail.com ', ' Some '),
+    (5, ' first @gmail.com ', ' First '),
+    (6, ' primero @gmail.com ', ' Primero '),
+    (7, ' segundo @gmail.com ', ' Segundo '),
+    (8, ' tercero @gmail.com ', ' Tercero '),
+    (9, ' cuarto @gmail.com ', ' Cuarto ');
---
+++ simple/src/test/java/tup/simple/SimpleApplicationTests.java
@@ -0,0 +1,13 @@
+package tup.simple;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SimpleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
