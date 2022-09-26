74d79a735b4d0d01d5f67fd491ced1c233181596
gsga <ggarciaa@gmail.com>
2022-09-26 18:12:53 -0300

    2022-09-25

--- simple/src/main/java/tup/simple/models/User.java
+++ simple/src/main/java/tup/simple/models/User.java
@@ -5,6 +5,9 @@ import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 
+import lombok.Data;
+
+@Data
 @Entity // This tells Hibernate to make a table out of this class
 public class User {
 
@@ -16,32 +19,32 @@ public class User {
 
     private String email;
 
-    public Long getId() {
-        return id;
-    }
+    // public Long getId() {
+    //     return id;
+    // }
 
-    public void setId(Long id) {
-        this.id = id;
-    }
+    // public void setId(Long id) {
+    //     this.id = id;
+    // }
 
-    public String getName() {
-        return name;
-    }
+    // public String getName() {
+    //     return name;
+    // }
 
-    public void setName(String name) {
-        this.name = name;
-    }
+    // public void setName(String name) {
+    //     this.name = name;
+    // }
 
-    public String getEmail() {
-        return email;
-    }
+    // public String getEmail() {
+    //     return email;
+    // }
 
-    public void setEmail(String email) {
-        this.email = email;
-    }
+    // public void setEmail(String email) {
+    //     this.email = email;
+    // }
 
-    @Override
-    public String toString() {
-        return "id=" + id + ", name=" + name + ", email=" + email;
-    }
+    // @Override
+    // public String toString() {
+    //     return "id=" + id + ", name=" + name + ", email=" + email;
+    // }
 }
