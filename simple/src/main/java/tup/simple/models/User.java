b38e6a15dc8df4848a8bdcdc5326e3ad805070e6
gsga <ggarciaa@gmail.com>
2022-09-29 16:08:06 -0300

    2022-09-29

--- simple/src/main/java/tup/simple/models/User.java
+++ simple/src/main/java/tup/simple/models/User.java
@@ -18,33 +18,4 @@ public class User {
     private String name;
 
     private String email;
-
-    // public Long getId() {
-    //     return id;
-    // }
-
-    // public void setId(Long id) {
-    //     this.id = id;
-    // }
-
-    // public String getName() {
-    //     return name;
-    // }
-
-    // public void setName(String name) {
-    //     this.name = name;
-    // }
-
-    // public String getEmail() {
-    //     return email;
-    // }
-
-    // public void setEmail(String email) {
-    //     this.email = email;
-    // }
-
-    // @Override
-    // public String toString() {
-    //     return "id=" + id + ", name=" + name + ", email=" + email;
-    // }
 }
